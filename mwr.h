53deaa72b122657341595263055b53802d25bd0a
Silas S. Brown <ssb22@cam.ac.uk>
2020-10-16 10:04:52 +0100

    change license to Apache 2

--- mwr.h
+++ mwr.h
@@ -3,27 +3,20 @@
      Algorithm for the Evaluation of Music at Pseudo-Random
      Idea Construction Stage
 
-   (C) 1996-2001 Silas S. Brown (St John's College,
-       Cambridge, UK, http://www.cus.cam.ac.uk/~ssb22 )
+   (C) 1996-2001 Silas S. Brown http://ssb22.user.srcf.net
     This code is no longer being maintained by the author.
 
-    This code was written in 1996.  I am aware that it could be better,
-    but do not have time to re-write it all.
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; see the file COPYING.  If not, write to
-    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-    Boston, MA 02111-1307, USA.
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+    
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
 */
 #ifndef MWR_H
 #define MWR_H
